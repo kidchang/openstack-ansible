ae8468ad10f5e820fb04991df9437dcdd5529b99
Jerry Zhao <zhaoxinyu@huawei.com>
2015-03-04 15:08:56 +0800

    create image from file instead of url
    in order to make sure image is created before other notify actions
    get executed, restarting mysql for example, which will interrupt
    the image creation, create image from file will make sure the image
    is active before script continues rather than creating from url
    asynchronously.
    
    closes #3

--- roles/glance/templates/image_upload.sh
+++ roles/glance/templates/image_upload.sh
@@ -1,1 +1,1 @@
-glance --os-username=admin --os-password={{ ADMIN_PASS }} --os-tenant-name=admin --os-auth-url=http://controller:35357/v2.0 image-create --name="{{ build_in_image_name }}" --disk-format=qcow2 --container-format=bare --is-public=true --copy-from {{ build_in_image }} && touch glance.import.completed
+glance --os-username=admin --os-password={{ ADMIN_PASS }} --os-tenant-name=admin --os-auth-url=http://controller:35357/v2.0 image-create --name="cirros" --disk-format=qcow2 --container-format=bare --is-public=true < /opt/{{ build_in_image_name }} && touch glance.import.completed
